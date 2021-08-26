b788d327add11b35afc55ae507fb24aae6a5994c
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2021-08-26 12:55:53 -0400

    changed callback for Facebook OAuth strategy

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new FacebookStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    callbackURL: "https://polar-oasis-13219.herokuapp.com/auth/facebook/secrets",
     // FOR GOOGLE ---> userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
   },
   function(accessToken, refreshToken, profile, cb) {
