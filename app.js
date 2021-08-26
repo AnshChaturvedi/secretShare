ead7b3f8454e7471056d94d034f3343640e64a52
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2021-08-26 11:04:50 -0400

    OAuth authenication with Facebook/Google

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const encrypt = require("mongoose-encryption");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -29,20 +31,52 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new FacebookStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    // FOR GOOGLE ---> userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ username: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get('/auth/facebook', 
+    passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.route("/register")
 
@@ -90,17 +124,42 @@ app.route("/login")
 
 app.route("/secrets")
     .get((req, res) => {
-        if (req.isAuthenticated()) {
-            res.render("secrets");
-        } else {
-            res.redirect("/login");
-        }
+        User.find({secret: {$ne:null}}, (err, foundUsers) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        })
     });
 
 app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
     });
+
+app.route("/submit")
+
+    .get((req, res) => {
+        if (req.isAuthenticated()) {
+            res.render("submit");
+        } else {
+            res.redirect("/login");
+        }
+    })
+
+    .post((req, res) => {
+        const userSubmittedSecret = req.body.secret;
+        
+        User.findById(req.user.id, (err, foundUser) => {
+            if (foundUser) {
+                foundUser.secret = userSubmittedSecret;
+                foundUser.save().then(() => {
+                    res.redirect("/secrets");
+                })
+            }
+        })
+    })
 
 app.listen(3000, (req, res) => {
     console.log("Server listening on port 3000");
