16bfacc034687d7f5b5de92328040ec5e7de3585
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2021-08-26 11:08:41 -0400

    added procfile

--- app.js
+++ app.js
@@ -161,6 +161,6 @@ app.route("/submit")
         })
     })
 
-app.listen(3000, (req, res) => {
+app.listen(process.env.PORT || 3000, (req, res) => {
     console.log("Server listening on port 3000");
 })
